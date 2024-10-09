d62dd320787d3344f616c36ef00c1fef8d457b4b
Arthur <arthur.hoshino@gmail.com>
2024-10-09 15:18:15 -0300

    timer do projeto

--- resources/views/components/projects/simple-card.blade.php
+++ resources/views/components/projects/simple-card.blade.php
@@ -14,20 +14,10 @@
                     Publicado {{ $project->created_at->diffForHumans() }}
                 </div>
 
-                <div class="flex items-center justify-between mt-4 text-[14px]">
-                    <div class="text-[#8C8C9A]  leading-6">Encerra em:</div>
-                    <div class="font-bold flex items-center space-x-1">
-                        <span class="text-white ">01</span><span>:</span>
-                        <span class="text-white ">12</span><span>:</span>
-                        <span class="text-white ">26</span><span>:</span>
-                        <span class="text-white ">64</span>
-                    </div>
-                </div>
+                <livewire:projects.timer :$project />
             </div>
         </div>
-
     </div>
-
 
     <div class="py-4 space-y-4">
         <div class="uppercase font-bold text-[#8C8C9A] text-[12px]">Tecnologias</div>
